18589e692cc4ad5d2fa15af764fc6368ba88141f
Ryan Milne <ryanmilne101@gmail.com>
2020-08-10 13:38:57 -0600

    update

--- src/components/HackerNews/HackerNews.js
+++ src/components/HackerNews/HackerNews.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Card from './../shared/Card/Card';
 import Loading from './../shared/Loading/Loading';
+import {connect}from 'react-redux'
+import {requestArticles} from '../../redux/hackerNewsReduce'
 
 class HackerNews extends Component {
   constructor(props) {
@@ -8,18 +10,26 @@ class HackerNews extends Component {
     this.state = { articles: [], loading: true }
   }
 
+  componentDidMount(){
+    this.props.requestArticles()
+    
+  }
+
   render() {
-    const articles = this.state.articles.map((article => <Card key={article.id} article={article} />))
+    const articles = this.props.hackerNews.articles.map((article => <Card key={article.id} article={article} />))
     return (
       <div className='news-container'>
         <img style={styles.logo} src="./hackerNews.jpeg" alt="" />
-        {this.state.loading ? <Loading /> : <div>{articles}</div>}
+        {this.props.hackerNews.loading ? <Loading /> : <div>{articles}</div>}
       </div>
     )
   }
 }
 
-export default HackerNews;
+const mapStateToProps = state => state
+
+
+export default connect(mapStateToProps, {requestArticles})(HackerNews)
 
 
 const styles = {
